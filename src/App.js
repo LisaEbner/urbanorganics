cbdad3d7e9422678f8b3ecd776781e350ac51ee0
unknown <lisaebner@gmail.com>
2019-12-07 16:30:24 -0800

    added image file

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Footer from "./components/Footer";
+import CreateUser from "./components/CreateUser";
+
+function App() {
+  return (
+    <div>
+      <CreateUser />
+      <Footer />
+    </div>
+
+  );
+}
+
+export default App;
---
+++ src/components/Footer/index.js
@@ -0,0 +1,28 @@
+import React from "react";
+
+function Footer() {
+  const myStyle = {
+    lineHeight: "2rem",
+    backgroundColor: "#32a8a4",
+
+  };
+  return (
+    <nav style={
+      myStyle
+    }
+      className="navbar fixed-bottom text-light" >
+
+      <a className="text-dark h3 text-left"
+        href="/" > Urban Organics </a>
+
+      <ul className="text-center list-inline" >
+        <li className="list-inline-item" > Created by: </li>
+        <li className="list-inline-item" > Austin Fletcher </li>
+        <li className="list-inline-item" > Lisa Ebner </li>
+        <li className="list-inline-item" > Wesley Clements </li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Footer;
