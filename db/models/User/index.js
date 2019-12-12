bde2be66598af57645800dbb4127c1704c3e01dc
Wesley Clements <wesley.clements@gmail.com>
2019-12-12 10:46:32 -0800

    refactored create user route

--- db/models/User/index.js
+++ db/models/User/index.js
@@ -30,4 +30,32 @@ UserSchema.virtual('fullName').get(() => {
   return `${this.name.first} ${this.name.last}`;
 })
 
+const validator = require('validator');
+const strongPassword = new RegExp(/^(?!.*\s)(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[!@#%&+=|^$*-]).{8,}/g);
+
+UserSchema.statics.parse = (data) => {
+  return new Promise((resolve, reject) => {
+    if (!data) reject("Bad Request");
+
+    const { email, password, name } = data;
+    if (typeof email !== "string" || typeof password !== "string") reject("Bad Request");
+    if (typeof name !== "object" || Array.isArray(name)) reject("Bad Request");
+
+    const { first, last } = name;
+    if (typeof first !== "string" || typeof last !== "string") reject("Bad Request");
+
+    if (!validator.isEmail(email)) resolve();
+    if (!password.match(strongPassword)) resolve();
+    if (first.length < 1 || last.length < 1) resolve();
+
+    resolve(
+      {
+        email,
+        password,
+        name: { first, last }
+      }
+    );
+  });
+}
+
 module.exports = UserSchema;
--- routes/api/users/create/index.js
+++ routes/api/users/create/index.js
@@ -1,79 +1,60 @@
 const { Router } = require('express');
 const router = Router();
 
-const validator = require('validator');
 const bcrypt = require('bcrypt');
 
-const strongPassword = new RegExp(/^(?!.*\s)(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[!@#%&+=|^$*-]).{8,}/g);
-
-function parseUser({ email, password, name }) {
-  if (typeof email !== "string" || typeof password !== "string") throw Error(400);
-
-  if (typeof name !== "object" || Array.isArray(name)) throw Error(400);
-  const { first, last } = name;
-  if (typeof first !== "string" || typeof last !== "string") throw Error(400);
-
-  if (!validator.isEmail(email) || !password.match(strongPassword)) throw Error(422);
-
-  if (first.length < 1 || last.length < 1) throw Error(422);
-
-  return {
-    email,
-    password,
-    name: { first, last }
-  };
-}
-
 router.post("/", (req, res) => {
-  try {
-    const { email, password, name } = parseUser(req.body);
-    const db = req.app.get('db');
+  const db = req.app.get('db');
+  db.User.parse(req.body)
+    .then(userData => {
+      if (!userData) throw "Unprocessable Entity";
+      return userData
+    })
+    .then(({ email, password, name }) => {
+      db.User.findOne({ email })
+        .then(user => {
+          if (user) {
+            return res.status(422).json({
+              status: 422,
+              statusText: "Unprocessable Entity"
+            });
+          }
+          const saltRounds = 10;
+          bcrypt.hash(password, saltRounds)
+            .then(hash => {
+              db.User.create(
+                {
+                  name,
+                  email,
+                  password: hash
+                }
+              )
+                .then(result => {
+                  console.log(result);
 
-    db.User.findOne({ email })
-      .then(user => {
-        if (user) {
+                  res.status(201).json({
+                    status: 201,
+                    statusText: "Created"
+                  })
+                });
+            });
+        });
+    })
+    .catch(err => {
+      switch (err) {
+        case "Unprocessable Entity":
           return res.status(422).json({
             status: 422,
             statusText: "Unprocessable Entity"
           });
-        }
-        const saltRounds = 10;
-        bcrypt.hash(password, saltRounds)
-          .then(hash => {
-            db.User.create(
-              {
-                name,
-                email,
-                password: hash
-              }
-            )
-              .then(result => {
-                console.log(result);
-
-                res.status(201).json({
-                  status: 201,
-                  statusText: "Created"
-                })
-              });
+        case "Bad Request":
+        default:
+          return res.status(400).json({
+            status: 400,
+            statusText: "Bad Request"
           });
-      });
-  } catch (err) {
-    switch (err.message) {
-      case "422":
-        res.status(422).json({
-          status: 422,
-          statusText: "Unprocessable Entity"
-        });
-        break;
-      case "400":
-      default:
-        res.status(400).json({
-          status: 400,
-          statusText: "Bad Request"
-        });
-        break;
-    }
-  }
+      }
+    });
 })
 
 module.exports = router;
