f1e17c68f9fde604380c4b81b3909144ce94d6e2
Wesley Clements <wesley.clements@gmail.com>
2019-12-12 10:09:10 -0800

    added user input validation for login and create user routes

--- routes/api/users/create/index.js
+++ routes/api/users/create/index.js
@@ -4,60 +4,76 @@ const router = Router();
 const validator = require('validator');
 const bcrypt = require('bcrypt');
 
-function getUser(body) {
-  const { name, email, password } = body;
-  if (!name || typeof email !== "string" || typeof password !== "string") return;
+const strongPassword = new RegExp(/^(?!.*\s)(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[!@#%&+=|^$*-]).{8,}/g);
+
+function parseUser({ email, password, name }) {
+  if (typeof email !== "string" || typeof password !== "string") throw Error(400);
+
+  if (typeof name !== "object" || Array.isArray(name)) throw Error(400);
   const { first, last } = name;
-  if (typeof first !== "string" || typeof last !== "string") return;
+  if (typeof first !== "string" || typeof last !== "string") throw Error(400);
+
+  if (!validator.isEmail(email) || !password.match(strongPassword)) throw Error(422);
+
+  if (first.length < 1 || last.length < 1) throw Error(422);
+
   return {
-    name: { first, last },
     email,
-    password
+    password,
+    name: { first, last }
   };
 }
 
 router.post("/", (req, res) => {
-  const user = getUser(req.body);
+  try {
+    const { email, password, name } = parseUser(req.body);
+    const db = req.app.get('db');
 
-  if (!user) {
-    return res.status(400).json({
-      status: 400,
-      statusText: "Bad Request"
-    });
-  }
+    db.User.findOne({ email })
+      .then(user => {
+        if (user) {
+          return res.status(422).json({
+            status: 422,
+            statusText: "Unprocessable Entity"
+          });
+        }
+        const saltRounds = 10;
+        bcrypt.hash(password, saltRounds)
+          .then(hash => {
+            db.User.create(
+              {
+                name,
+                email,
+                password: hash
+              }
+            )
+              .then(result => {
+                console.log(result);
 
-  const { name, email, password } = user;
-
-  const db = req.app.get('db');
-
-  db.User.findOne({ email })
-    .then(user => {
-      if (user) {
-        return res.status(422).json({
+                res.status(201).json({
+                  status: 201,
+                  statusText: "Created"
+                })
+              });
+          });
+      });
+  } catch (err) {
+    switch (err.message) {
+      case "422":
+        res.status(422).json({
           status: 422,
           statusText: "Unprocessable Entity"
         });
-      }
-      const saltRounds = 10;
-      bcrypt.hash(password, saltRounds)
-        .then(hash => {
-          db.User.create(
-            {
-              name,
-              email,
-              password: hash
-            }
-          )
-            .then(result => {
-              console.log(result);
-
-              res.status(200).json({
-                status: 200,
-                statusText: "OK"
-              })
-            });
+        break;
+      case "400":
+      default:
+        res.status(400).json({
+          status: 400,
+          statusText: "Bad Request"
         });
-    });
+        break;
+    }
+  }
 })
 
 module.exports = router;
--- routes/api/users/login/index.js
+++ routes/api/users/login/index.js
@@ -1,57 +1,64 @@
-const jwt = require('jsonwebtoken')
-const bcrypt = require('bcrypt');
-
 const { Router } = require('express');
 const router = Router();
 
-// Settings for JWT signing
-const SIGNING_CONFIG = {
-  algorithm: "HS256",
-  expiresIn: "48h"
+const validator = require('validator');
+const jwt = require('jsonwebtoken')
+const bcrypt = require('bcrypt');
+
+function parseLogin({ email, password }) {
+  if (typeof email !== "string" || typeof password !== "string") throw Error();
+  if (!validator.isEmail(email)) throw Error();
+  return {
+    email,
+    password
+  };
 }
 
 router.post("/", (req, res) => {
-  const { email, password } = req.body;
-  // If email
-  if (typeof email !== "string" || typeof password !== "string") {
-    return res.status(400).json({
+  try {
+    const { email, password } = parseLogin(req.body);
+    req.app.get('db').User.findOne({ email })
+      .then(user => {
+        if (user === null) throw Error();
+
+        bcrypt.compare(password, user.password)
+          .then(isSame => {
+            if (!isSame) throw Error();
+
+            res.cookie(
+              "authToken",
+              jwt.sign(
+                { id: user._id },
+                process.env.PRIVATE_AUTH_KEY,
+                {
+                  algorithm: "HS256",
+                  expiresIn: "48h"
+                }
+              ),
+              {
+                httpOnly: true,
+                //secure: true
+              }
+            );
+            res.status(200).json({
+              status: 200,
+              statusText: "OK"
+            });
+          });
+      })
+      .catch(err => {
+        return res.status(400).json({
+          status: 401,
+          statusText: "Unauthorized"
+        });
+      });
+  }
+  catch {
+    res.status(400).json({
       status: 400,
       statusText: "Bad Request"
     });
   }
-
-  req.app.get('db').User.findOne({ email })
-    .then(user => {
-      if (user === null) throw Error();
-
-      bcrypt.compare(password, user.password)
-        .then(response => {
-          if (!response) throw Error();
-
-          res.cookie(
-            "authToken",
-            jwt.sign(
-              { email },
-              process.env.PRIVATE_AUTH_KEY,
-              SIGNING_CONFIG
-            ),
-            {
-              httpOnly: true,
-              //secure: true
-            }
-          );
-          res.status(200).json({
-            status: 200,
-            statusText: "OK"
-          });
-        });
-    })
-    .catch(err => {
-      return res.status(400).json({
-        status: 401,
-        statusText: "Unauthorized"
-      });
-    });
 });
 
 module.exports = router;
