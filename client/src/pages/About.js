b481a2ac9ce8e7509bd3a5615f8ab290d1adcd00
Afletch <austinj.fletcher@gmail.com>
2019-12-18 17:10:36 -0800

    create about carousel

---
+++ client/src/pages/About.js
@@ -0,0 +1,79 @@
+import React from "react";
+function About() {
+
+
+  return (
+    <div style={{ backgroundColor: "rgba(85,239,196, 0.85)", fontFamily: "Lora, serif" }}>
+
+      <h1 style={{ padding: "3rem 0" }}>About Us</h1>
+      <br></br>
+
+      <div id="carouselExampleIndicators" className="carousel slide" data-ride="carousel" style={{ height: "40vh" }}>
+        <ol className="carousel-indicators">
+          <li data-target="#carouselExampleIndicators" data-slide-to="0" className="active"></li>
+          <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+          <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+        </ol>
+        <div className="carousel-inner">
+          <div className="carousel-item active" style={{ padding: "0 15vw" }}>
+            <h2>
+              Better farms, better quality, better products. That’s our promise.</h2>
+            <br></br>
+            <p style={{ fontSize: "1.5rem", }}>
+              Urban Organics is a Portland, Oregon based meal planning service
+              delivering seasonal, organic, wellness smoothies for affordable prices.
+              Designed for competitive athletes, these specially formulated blends are
+              optimized to fuel your performance, promote wellness and provide maximum
+              nutritional support. We reveal the ingredients behind every product from
+              our ethical manufacturing process to our transparent pricing breakdown.
+              Urban Organics is a wellness brand that sparks joy inspires simplifies
+              and simplifies healthful living through freshly blended whole fruit,
+              leafy greens, vegetables, and powerful antioxidants.
+      </p>
+          </div>
+          <div className="carousel-item " style={{ padding: "0 15vw" }}>
+            <h2>Why Urban Organics?</h2>
+            <br></br>
+            <strong>Stay Fit</strong>
+            <br></br>
+            <p style={{ fontSize: "1.5rem" }}>
+              Traditional restaurant fare is associated with a daily increase in
+              calories, fat, saturated fat and sodium. Studies have shown that fat
+              mass increases and muscle mass decreases with age. Smart competitors
+              know that abs are made in the kitchen.
+      </p>
+            <p style={{ fontSize: "1rem" }}>Save time and keep your meal budget on track By signing up for our
+              affordable weekly meal delivery service, you create a predictable food
+              budget and sustainable diet without the hassle of shopping, chopping,
+        and cleaning up the kitchen.</p>
+          </div>
+          <div className="carousel-item" style={{ padding: "0 15vw" }}>
+            <h2>Our Plan</h2>
+            <strong>
+              Be part of a solution 5% of all profits provide support for the Portland
+              homeless community
+      </strong>
+            <strong>Employee Benefits & Wellness Plans</strong>
+            <p className="">
+              Attract and maintain top talent with our employee wellness plans.
+              Portland’s highest-rated employers care not just about the bottom line,
+              but employee satisfaction, and wellbeing. Contact us today to discuss
+              your custom catering needs.
+      </p>
+          </div>
+        </div>
+        <a className="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+          <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+          <span className="sr-only">Previous</span>
+        </a>
+        <a className="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+          <span className="carousel-control-next-icon" aria-hidden="true"></span>
+          <span className="sr-only">Next</span>
+        </a>
+      </div>
+
+    </div>
+  );
+}
+
+export default About;
--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,7 +1,8 @@
 import React from "react";
+import About from "./About";
 function Home(props) {
   const perfectImageStyle = {
-    backgroundImage: `url(./images/homepage-avacadomango-unsplash.jpg)`,
+    backgroundImage: `url(./logos/background_concept2.jpg)`,
     backgroundPosition: "center center",
     backgroundRepeat: "no-repeat",
     backgroundAttachment: "fixed",
@@ -11,30 +12,32 @@ function Home(props) {
 
 
   return (
-    <div style={perfectImageStyle} className="homePageImage text-center">
-      <div style={{ display: "flex", flexDirection: "column", height: "100vh", justifyContent: "space-between", alignItems: "center" }}>
+    <div>
+      <div style={perfectImageStyle} className="homePageImage text-center">
+        <div style={{ display: "flex", flexDirection: "column", height: "100vh", justifyContent: "space-between", alignItems: "center" }}>
 
-        <div style={{ paddingTop: "10rem" }} className="name-and-logo">
+          <div style={{ paddingTop: "10rem" }} className="name-and-logo">
 
-          <div className="urbanOrganicsLogo mx-auto">
-            <img src="./logos/UrbOrgLogo.png" style={{ height: "250px", width: "250px" }}></img>
+            <div className="urbanOrganicsLogo mx-auto">
+              <img src="./logos/UrbOrgLogo.png" style={{ height: "250px", width: "250px" }}></img>
+            </div>
+
+            <h1 style={{ fontFamily: "Montserrat, sans-serif" }} className="">Urban Organics</h1>
+
           </div>
 
-          <h1 className="">Urban Organics</h1>
+          <div style={{ height: "auto", width: "fit-content", border: "solid lightgray 1px", background: "black", opacity: "0.5", }} onClick={() => props.handlePageChange("Store")} className="btn pb-2 pt-2 pr-4 pl-4">
+            <h2 className="text-light mb-0">GO SHOPPING</h2>
+            <h2 className="text-light">------></h2>
+          </div>
+
+          <div className="btn" id="arrowButton">
+            <img src="./logos/downarrow3.png" alt="down arrow" className="flex-bottom" style={{ height: "50px", width: "80px" }}></img>
+          </div>
 
         </div>
-
-        <div style={{ height: "auto", width: "fit-content", border: "solid lightgray 1px", background: "black", opacity: "0.5", }} onClick={() => props.handlePageChange("Store")} className="btn pb-2 pt-2 pr-4 pl-4">
-          <h2 className="text-light mb-0">GO SHOPPING</h2>
-          <h2 className="text-light">------></h2>
-        </div>
-
-        <div className="btn" id="arrowButton">
-          <img src="./logos/downarrow.png" alt="down arrow" className="flex-bottom" style={{ height: "30px", width: "60px" }}></img>
-        </div>
-
+        <About />
       </div>
-
     </div>
   );
 }
